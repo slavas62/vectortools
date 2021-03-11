4ff76eef719f9434395758aad93443d2e0d0eb3b
slavas <slavas62@ya.ru>
2021-03-11 14:28:07 +0300

    Меняем url репозитория

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='vectortools',
     version='0.0.7',
     long_description=long_description,
-    url='https://bitbucket.org/slavas/vectortools/',
+    url='https://github.com/slavas62/vectortools/',
     packages=['vectortools', 'vectortools.django'],
     description='Tools for processing different vector formats',
     install_requires=[
